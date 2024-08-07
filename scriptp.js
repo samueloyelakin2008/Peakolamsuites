e3e96038a50152d76506fb888099f45039582af2
samueloyelakin2008 <166112795+samueloyelakin2008@users.noreply.github.com>
2024-08-07 20:26:39 +0100

    Update scriptp.js

--- scriptp.js
+++ scriptp.js
@@ -72,7 +72,7 @@ function sendToWhatsApp(params) {
                     `Check-out: ${params.check_out}\n` +
                     `Message: ${params.message}`;
     
-    const whatsappNumber = "+2347045247679";
+    const whatsappNumber = "+2348037204293";
     const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
 
     // Open WhatsApp chat
